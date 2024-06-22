f4a465ae8b720e82d999ea9ac2d60a0b579f6d02
onurbeht <brunnobatista25@gmail.com>
2024-06-21 21:09:00 -0300

    First commit

---
+++ src/main/java/oak/bruno/challenge/ChallengeApplication.java
@@ -0,0 +1,20 @@
+package oak.bruno.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeApplication.class, args);
+	}
+
+}
+
+
+//todo - Formulário com os campos abaixo:
+//todo - Nome do produto - campo de texto
+//todo - Descrição do produto - campo de texto
+//todo  - Valor do produto - campo de valor
+//todo - Disponível para venda - campo com 2 opções: sim / não
